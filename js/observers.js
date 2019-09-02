4f6fdb28874a6de9e57590f652dc95d6b4b4887b
AbrahamDN <40493309+AbrahamDN@users.noreply.github.com>
2019-09-02 23:11:52 +0100

    intial upload

---
+++ js/observers.js
@@ -0,0 +1,50 @@
+
+function sectionOneObserver() {
+
+    const header = document.querySelector('header');
+    const sectionOne = document.querySelector('.home-intro');
+
+    const sectionOneOptions = {
+        rootMargin: '-120px 0px 0px 0px'
+    };
+
+    const sectionOneObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting)
+                header.classList.add('nav-scrolled');
+            else
+                header.classList.remove('nav-scrolled');
+        })
+    }, sectionOneOptions);
+    sectionOneObserver.observe(sectionOne);
+
+}
+sectionOneObserver();
+
+
+function appearOnScroll() {
+    const faders = document.querySelectorAll('.fade-in');
+    const sliders = document.querySelectorAll('.slide-in');
+
+    const appearOptions = {
+        threshold: 0,
+        rootMargin: '0px 0px -200px 0px'
+    };
+    const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) return;
+            else {
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+    sliders.forEach(slider => {
+        appearOnScroll.observe(slider);
+    });
+}
+appearOnScroll();
